fd3578be871f2c195b43c746f8a68db01a94cb2c
jafferrilwaan-png <jafferrilwaan@gmail.com>
2026-09-06 12:43:23 +0530

    fix: add missing useEffect import in SubterraneanTheatreMap

--- src/components/SubterraneanTheatreMap.tsx
+++ src/components/SubterraneanTheatreMap.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TopoContour from "./TopoContour";
 import { TelemetryPayload } from "./TacticalC2Dashboard";
 import { 
